4783de7a08133c85e2e5875e6f257ddfdc965238
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-22 15:26:43 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBoQ5ntVNxXENwRkAVEA-qJf4iRKoE76lk",
-  authDomain: "tutorial-ea726.firebaseapp.com",
-  projectId: "tutorial-ea726",
-  storageBucket: "tutorial-ea726.appspot.com",
-  messagingSenderId: "1048781684417",
-  appId: "1:1048781684417:web:8b78ace85c33ab94b29e40",
-  measurementId: "G-LNF7ZN2T25"
+  apiKey: "AIzaSyC-ZtQ4N3d5ZYZcUCETe0quVgn3wD8Gdq4",
+  authDomain: "datagram-in.firebaseapp.com",
+  projectId: "datagram-in",
+  storageBucket: "datagram-in.appspot.com",
+  messagingSenderId: "984986151374",
+  appId: "1:984986151374:web:8428c23ad9e8bd88ee639f",
+  measurementId: "G-N6T4KFH17X"
   };
 
   // Initialize Firebase
